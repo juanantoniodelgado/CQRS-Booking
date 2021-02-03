d0383e784de870740cf69f239cfa2febcb93bdc0
Juan Antonio Delgado <juanantonio.delgadolpez@gmail.com>
2021-02-03 18:55:10 +0100

    Push for collaborator

---
+++ tests/Unit/Application/Booking/CheckRoomAvailabilityTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Application\Booking;
+
+use PHPUnit\Framework\TestCase;
+
+class CheckRoomAvailabilityTest extends TestCase
+{
+
+}
---
+++ tests/Unit/Domain/RoomTest.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Tests\Unit\Domain;
+
+
+class RoomTest
+{
+
+}
